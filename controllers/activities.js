1f11f8972da456c8d827359cc0ab06df52577406
Ruthie McKinney <ruthie@schnopp365.com>
2021-07-26 18:18:56 +0200

    validation for existing activity

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,9 +1,36 @@
 import activities from "../data/activities.js";
+import { validationResult } from "express-validator";
+
+const checkIfActivtyInArray = (id) => {
+    if (!id) return null;
+    const sanitisedEntry = id.toUpperCase();
+    return activities.find(
+        (activities) =>
+            activities.id === sanitisedEntry ||
+            activities.name === sanitisedEntry
+    );
+};
 
 export const getAll = (req, res) => res.json(activities);
-
 export const getSingle = (req, res) => res.json("Get single activity");
+export const createActivty = (req, res) => {
+    const [id, name] = req.body;
 
-export const createActivty = (req, res) => res.json("Create single activity");
+    const alreadyExists = [
+        checkIfActivtyInArray(id),
+        checkIfActivtyInArray(name),
+    ];
+
+    if (alreadyExists.some((Element) => Element))
+        return res.status(404).json({ error: "Activity already exists" });
+
+    const newActivity = {
+        id: activities.length + 1,
+        ...req.body,
+    };
+    activities.push(newActivity);
+    res.json(newActivity);
+};
+
 export const updateActivity = (req, res) => res.json("Update single activity");
 export const deleteActivty = (req, res) => res.json("Delete single activity");
